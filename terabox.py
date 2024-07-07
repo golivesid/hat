6fcf6a988961d6d1e87a3fc03e1741287937f752
Flixjod <144277345+Flixjod@users.noreply.github.com>
2024-07-08 00:15:53 +0530

    Update terabox.py

--- terabox.py
+++ terabox.py
@@ -144,10 +144,7 @@ def ban_user(message):
         return
 
     user_id_to_ban = int(message.text.split()[1])
-    try:
-        user_info = bot.get_chat(user_id_to_ban)
-
-    
+    user_info = bot.get_chat(user_id_to_ban)
     if banned_users_collection.find_one({'user_id': user_id_to_ban}):
         bot.reply_to(message, f"ᴜꜱᴇʀ <a href='tg://user?id={user_id_to_ban}'>{user_info.first_name}</a> ɪꜱ ᴀʟʀᴇᴀᴅʏ ʙᴀɴɴᴇᴅ.", parse_mode='HTML')
         return
@@ -167,13 +164,13 @@ def unban_user(message):
         return
 
     user_id_to_unban = int(message.text.split()[1])
-
+    user_info = bot.get_chat(user_id_to_ban)
     if not banned_users_collection.find_one({'user_id': user_id_to_unban}):
-        bot.reply_to(message, f"ᴜꜱᴇʀ <a href='tg://user?id={user_id_to_unban}'>{bot.get_chat(user_id_to_unban).first_name}</a> ɪꜱ ɴᴏᴛ ᴄᴜʀʀᴇɴᴛʟʏ ʙᴀɴɴᴇᴅ.", parse_mode='HTML')
+        bot.reply_to(message, f"ᴜꜱᴇʀ <a href='tg://user?id={user_id_to_unban}'>{user_info.first_name}</a> ɪꜱ ɴᴏᴛ ᴄᴜʀʀᴇɴᴛʟʏ ʙᴀɴɴᴇᴅ.", parse_mode='HTML')
         return
 
     banned_users_collection.delete_one({'user_id': user_id_to_unban})
-    bot.reply_to(message, f"ᴜꜱᴇʀ <a href='tg://user?id={user_id_to_unban}'>{bot.get_chat(user_id_to_unban).first_name}</a> ʜᴀꜱ ʙᴇᴇɴ ᴜɴʙᴀɴɴᴇᴅ.", parse_mode='HTML')
+    bot.reply_to(message, f"ᴜꜱᴇʀ <a href='tg://user?id={user_id_to_unban}'>{user_info.first_name}</a> ʜᴀꜱ ʙᴇᴇɴ ᴜɴʙᴀɴɴᴇᴅ.", parse_mode='HTML')
 
 # Handle messages
 @bot.message_handler(func=lambda message: True)
